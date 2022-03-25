d2a60d8ca4ccab68c2962666213695df102b0255
sumanthdaruvuri <sumanth.daruvuri@gmail.com>
2022-03-26 00:08:29 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dsi9xy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dsi9xy)
---
+++ src/app/summys.ts
@@ -0,0 +1,14 @@
+export interface Summy{
+  name: string;
+  rollno: number;
+}
+export const summys = [
+  {
+    name: 'sumanth',
+    rollno: 23,
+  },
+  {
+    name: 'lavanya',
+    rollno: 46,
+  },
+];
